67fb1ee55845de1e886ab3ae54db5f0b4c62e27b
Chaim B. Sommers <cb@compute.co.il>
2021-11-30 09:42:48 +0200

    Structural setup. Gulp task setup.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 // Sass configuration
 var gulp = require('gulp');
+var rename = require("gulp-rename");
 var sass = require('gulp-sass')(require('sass'));
+var template = require('gulp-template-html');
 
-gulp.task('sass', function(cb) {
+gulp.task('sass', function (cb) {
   gulp
     .src('scss/*.scss')
     .pipe(sass())
@@ -12,10 +14,18 @@ gulp.task('sass', function(cb) {
   cb();
 });
 
-gulp.task(
-  'default',
-  gulp.series('sass', function(cb) {
-    gulp.watch('scss/*.scss', gulp.series('sass'));
-    cb();
-  })
-);
+gulp.task('template-basic', function (cb) {
+  return gulp.src('content/**/*.content.basic.html')
+    .pipe(template('templates/template-basic.html'))
+    .pipe(rename(function (path) {
+      path.basename = path.basename.replace('.content.basic', '');
+    }))
+    .pipe(gulp.dest('./'));
+  cb();
+});
+
+
+gulp.task('default', function () {
+  gulp.watch(['scss/*.scss', 'scss/**/*.scss'], gulp.series('sass'));
+  gulp.watch(['templates/template-basic.html', 'content/**/*.content.basic.html'], gulp.series('template-basic'));
+});
---
+++ start.bat
@@ -0,0 +1,2 @@
+start cmd.exe /k "npx http-server"
+start cmd.exe /k "C:\Program Files\ngrok\ngrok.exe" http -subdomain=skf http://localhost:8080
